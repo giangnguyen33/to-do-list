49750166536bba20c4ad9ebd88ebcf50faf30e90
Giang Nguyen <gnguyen@Giangs-MBP.hub>
2023-03-21 23:53:08 +1100

    refactor

--- src/components/TodoList.test.tsx
+++ src/components/TodoList.test.tsx
@@ -25,17 +25,9 @@ describe('Todo List test',()=>{
 
   it('should render todoList',async()=>{
     render(<TodoList todos={todos}></TodoList>)
-    const item1 = screen.getByTestId(`todo-item-${todos[0].id}`);
-    expect(item1).toHaveTextContent('Buy groceries');
-    expect(getByRole(item1, 'checkbox')).not.toBeChecked()
-
-    const item2 = screen.getByTestId(`todo-item-${todos[1].id}`);
-    expect(item2).toHaveTextContent( "Reboot computer");
-    expect(getByRole(item2, 'checkbox')).not.toBeChecked()
-
-    const item3 = screen.getByTestId(`todo-item-${todos[2].id}`);
-    expect(item3).toHaveTextContent( "Ace CoderPad interview");
-    expect(getByRole(item3, 'checkbox')).toBeChecked()
+    checkTodoItem(todos[0]);
+    checkTodoItem(todos[1]);
+    checkTodoItem(todos[2]);
   })
 
   it('should change status of checked  when click on checkbox' ,async()=>{
@@ -52,3 +44,14 @@ describe('Todo List test',()=>{
   })
 
 })
+
+const checkTodoItem = (todo: Todo)=> {
+    const item = screen.getByTestId(`todo-item-${todo.id}`);
+    expect(item).toHaveTextContent(todo.label);
+    const checkbox = getByRole(item, 'checkbox');
+    if(todo.checked) {
+        expect(checkbox).toBeChecked();
+    } else {
+        expect(checkbox).not.toBeChecked();
+    }
+}
